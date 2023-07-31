761467fffd123246c5987de1119d7242e0fd36f0
Muhammad Ammad Sheikh <139677523+AmmadLala@users.noreply.github.com>
2023-07-31 14:52:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+const nameWSpace = '\t\n Muhammad Ammad \n\t';
+const namwoSpace = nameWSpace.trim();
+console.log(`Stripped:${nameWSpace}`);
+console.log(`without stripped:${namwoSpace}`);
---
+++ index.ts
@@ -0,0 +1,4 @@
+const nameWSpace = '\t\n Muhammad Ammad \n\t'
+const namwoSpace= nameWSpace.trim ();
+console.log(`Stripped:${nameWSpace}`);
+console.log(`without stripped:${namwoSpace}`);
